d8c7c2ab3aaa9fc596d05f3e9c3462fc5c87f0e8
Adelyne Chan <adelynechan@gmail.com>
2017-07-13 14:05:06 +0100

    own code test

--- server.R
+++ server.R
@@ -1,22 +1,16 @@
 shinyServer(function(input, output) {
   
+  microarray_data <- read.table("data/microarray.txt")
+  selected_gene_microarray_data <- microarray_data[microarray_data$ensembl_id == input,]
+  
   output$main_plot <- renderPlot({
     
-    hist(faithful$eruptions,
-         probability = TRUE,
-         breaks = as.numeric(input$n_breaks),
-         xlab = "Duration (minutes)",
-         main = "Geyser eruption duration")
-    
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
-    }
-    
-    if (input$density) {
-      dens <- density(faithful$eruptions,
-                      adjust = input$bw_adjust)
-      lines(dens, col = "blue")
-    }
+    ggplot(selected_genes_logfc, aes(x=sample, y=logfc, group=gene_symbol, color=gene_symbol)) 
+    + geom_point() + theme(axis.text.x=element_text(angle=270))
     
   })
+  
 })
+
+
+
